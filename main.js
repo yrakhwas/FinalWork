16bf78ace9a97562fe80f78495ed95bbbed6ba22
yrakhwas <yrakhwas@gmail.com>
2023-10-13 20:18:47 +0300

    add data to modal form for edit

--- main.js
+++ main.js
@@ -7,60 +7,16 @@ const editForm = document.getElementById("editForm");
 const savedCars = JSON.parse(localStorage.getItem("cars")) || [];
 const colorOptions = ["Червоний", "Синій", "Зелений", "Жовтий"];
 const transmitionOption = ["Automat", "Robot", "Manual"];
+const volumeOption = [0.9, 1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.2, 3.5,3.7,4.0]
+const saveButton = document.getElementById("saveButton");
 
-// Отримуємо посилання на елемент <select> за його ідентифікатором
-// const colourSelects = document.querySelectorAll(".colourSelect");
-// const transmitionSelect = document.querySelectorAll(".transmitionSelect");
 
 const colourSelectModal = document.getElementById("colourSelectModal");
 const colourSelectCol12 = document.getElementById("colourSelectCol12");
 const transmitionSelectModal = document.getElementById("transmitionSelectModal");
 const transmitionSelectCol12 = document.getElementById("transmitionSelectCol12");
-
-
-
-for (const transmition of transmitionOption) {
-  const optionElement = document.createElement("option");
-  optionElement.value = transmition; // Значення опції
-  optionElement.textContent = transmition; // Текст опції
-  transmitionSelectModal.appendChild(optionElement);
-  transmitionSelectCol12.appendChild(optionElement.cloneNode(true));
-}
-
-
-for (const colour of colorOptions) {
-  const optionElement = document.createElement("option");
-  optionElement.value = colour; // Значення опції
-  optionElement.textContent = colour; // Текст опції
-  colourSelectModal.appendChild(optionElement);
-  colourSelectCol12.appendChild(optionElement.cloneNode(true));
-}
-// Додавання опцій в <select> на основі масиву
-// colourSelects.forEach(function (select) {
-//   colorOptions.forEach(function (color) {
-//     const optionElement = document.createElement("option");
-//     optionElement.value = color; // Значення опції
-//     optionElement.textContent = color; // Текст опції
-//     select.appendChild(optionElement);
-//   });
-// });
-
-// Додавання опцій для кольору в модальному вікні
-colorOptions.forEach(function (color) {
-  const optionElement = document.createElement("option");
-  optionElement.value = color;
-  optionElement.textContent = color;
-  colourSelectModal.appendChild(optionElement);
-});
-
-// Додавання опцій для трансмісії в модальному вікні
-transmitionOption.forEach(function (transmition) {
-  const optionElement = document.createElement("option");
-  optionElement.value = transmition;
-  optionElement.textContent = transmition;
-  transmitionSelectModal.appendChild(optionElement);
-});
-
+const volumeSelectModal = document.getElementById("volumeSelectModal");
+const volumeSelectCol12 = document.getElementById("volumeSelectCol12");
 
 // Функція для оновлення таблиці на основі збережених даних
 function updateTable() {
@@ -103,6 +59,7 @@ function createEditButton(rowIndex) {
   editButton.addEventListener("click", function () {
     openModal(rowIndex);
   });
+  //editButton.setAttribute("data-index", rowIndex); // Додаємо атрибут з індексом
   return editButton;
 }
 
@@ -121,6 +78,9 @@ function editCar(rowIndex) {
   const carToEdit = savedCars[rowIndex];
   // Заповніть форму редагування з даними автомобіля
   fillEditForm(carToEdit);
+  openModal(rowIndex);
+
+  
 }
 
 // Функція для видалення автомобіля
@@ -132,9 +92,29 @@ function deleteCar(rowIndex) {
   updateTable();
 }
 
-function openModal()
+function openModal(rowIndex)
 {
   modal.style.display = "block";
+  const carToEdit = savedCars[rowIndex];
+
+  // // Створення об'єкта нового автомобіля
+  const editBrandInput = document.getElementById("editBrand");
+  const editModelInput = document.getElementById("editModel");
+  const editColourSelect = document.getElementById("colourSelectCol12");
+  const editCarSalonInput = document.getElementById("inputCarSalon");
+  const editYearInput = document.getElementById("editYear");
+  const editVolumeInput = document.getElementById("volumeSelectCol12");
+  const editTransmitionInput = document.getElementById("editTransmition");
+
+  // Заповніть поля вводу значеннями з автомобіля
+  editBrandInput.value = carToEdit.brand;
+  editModelInput.value = carToEdit.model;
+  colourSelectModal.value = carToEdit.colour;
+  editSalon.value = carToEdit.carSalon;
+  editYearInput.value = carToEdit.year;
+  volumeSelectModal.value = carToEdit.volume;
+  transmitionSelectModal.value = carToEdit.transmition;
+
 }
 
 function closeModal()
@@ -143,7 +123,9 @@ function closeModal()
 }
 // Викликати функцію для відображення збережених даних
 updateTable();
-
+closeModalBtn.addEventListener("click", function () {
+  closeModal();
+});
 // Реагування на подію подачі форми
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -154,7 +136,7 @@ form.addEventListener("submit", function (e) {
   const colour = document.getElementById("colourSelectCol12").value;
   const carSalon = document.getElementById("inputCarSalon").value;
   const year = document.getElementById("inputYear").value;
-  const volume = document.getElementById("inputVolume").value;
+  const volume = document.getElementById("volumeSelectCol12").value;
   const transmition = document.getElementById("transmitionSelectCol12").value;
 
   // Створення об'єкта нового автомобіля
@@ -183,4 +165,124 @@ closeModalBtn.addEventListener("click", function()
 
   // Очистка полів форми
   form.reset();
+});
+
+
+saveButton.addEventListener("click", function () {
+  // Отримайте значення полів форми редагування
+  const editedBrand = document.getElementById("editBrand").value;
+  const editedModel = document.getElementById("editModel").value;
+  const editedColour = document.getElementById("colourSelectModal").value;
+  const editedCarSalon = document.getElementById("editSalon").value;
+  const editedYear = document.getElementById("editYear").value;
+  const editedVolume = document.getElementById("volumeSelectModal").value;
+  const editedTransmition = document.getElementById("transmitionSelectModal").value;
+
+  // Отримайте індекс редагованого автомобіля (можливо ви вже зберігаєте його)
+  const editedCarIndex = parseInt(editButton.getAttribute("data-index"));
+
+  // Оновіть дані автомобіля в масиві `savedCars`
+  savedCars[editedCarIndex] = {
+    brand: editedBrand,
+    model: editedModel,
+    colour: editedColour,
+    carSalon: editedCarSalon,
+    year: editedYear,
+    volume: editedVolume,
+    transmition: editedTransmition
+  };
+
+  // Оновіть збережені дані в localStorage
+  localStorage.setItem("cars", JSON.stringify(savedCars));
+
+  // Оновіть таблицю для відображення оновлених даних
+  updateTable();
+
+  // Закрийте модальне вікно
+  closeModal();
+});
+
+
+
+editForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // Заборона дії за замовчуванням (відправки форми)
+
+  // Отримайте значення полів форми редагування
+  const editedBrand = document.getElementById("editBrand").value;
+  const editedModel = document.getElementById("editModel").value;
+  const editedColour = document.getElementById("colourSelectModal").value;
+  const editedCarSalon = document.getElementById("editSalon").value;
+  const editedYear = document.getElementById("editYear").value;
+  const editedVolume = document.getElementById("volumeSelectModal").value;
+  const editedTransmition = document.getElementById("transmitionSelectModal").value;
+
+  // Отримайте індекс редагованого автомобіля (можливо ви вже зберігаєте його)
+const editedCarIndex = parseInt(editButton.getAttribute("data-index"));
+
+  // Оновіть дані автомобіля в масиві `savedCars`
+  savedCars[editedCarIndex] = {
+    brand: editedBrand,
+    model: editedModel,
+    colour: editedColour,
+    carSalon: editedCarSalon,
+    year: editedYear,
+    volume: editedVolume,
+    transmition: editedTransmition
+  };
+
+  // Оновіть збережені дані в localStorage
+  localStorage.setItem("cars", JSON.stringify(savedCars));
+
+  // Оновіть таблицю для відображення оновлених даних
+  updateTable();
+
+  // Закрийте модальне вікно
+  closeModal();
+});
+
+for (const transmition of transmitionOption) {
+  const optionElement = document.createElement("option");
+  optionElement.value = transmition; // Значення опції
+  optionElement.textContent = transmition; // Текст опції
+  transmitionSelectModal.appendChild(optionElement);
+  transmitionSelectCol12.appendChild(optionElement.cloneNode(true));
+}
+
+
+for (const colour of colorOptions) {
+  const optionElement = document.createElement("option");
+  optionElement.value = colour; // Значення опції
+  optionElement.textContent = colour; // Текст опції
+  colourSelectModal.appendChild(optionElement);
+  colourSelectCol12.appendChild(optionElement.cloneNode(true));
+}
+
+for (const volume of volumeOption) {
+  const optionElement = document.createElement("option");
+  optionElement.value = volume; // Значення опції
+  optionElement.textContent = volume; // Текст опції
+  volumeSelectModal.appendChild(optionElement);
+  volumeSelectCol12.appendChild(optionElement.cloneNode(true));
+}
+// Додавання опцій для кольору в модальному вікні
+colorOptions.forEach(function (color) {
+  const optionElement = document.createElement("option");
+  optionElement.value = color;
+  optionElement.textContent = color;
+  colourSelectModal.appendChild(optionElement);
+});
+
+// Додавання опцій для трансмісії в модальному вікні
+transmitionOption.forEach(function (transmition) {
+  const optionElement = document.createElement("option");
+  optionElement.value = transmition;
+  optionElement.textContent = transmition;
+  transmitionSelectModal.appendChild(optionElement);
+});
+
+volumeOption.forEach(function (volume) {
+  const optionElement = document.createElement("option");
+  optionElement.value = volume;
+  optionElement.textContent = volume;
+  volumeSelectModal.appendChild(optionElement);
 });
